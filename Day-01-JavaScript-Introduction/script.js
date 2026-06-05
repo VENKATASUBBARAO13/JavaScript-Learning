46559848b89da42bbcfa9cbc66f8243973d12045
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-05 13:18:20 +0530

    Add files via upload

---
+++ Day-01-JavaScript-Introduction/script.js
@@ -0,0 +1,10 @@
+﻿// Day 1: JavaScript Introduction
+// JavaScript is used to make webpages interactive.
+
+console.log("Welcome to JavaScript learning!");
+
+const pageTitle = document.getElementById("page-title");
+const message = document.getElementById("message");
+
+pageTitle.textContent = "JavaScript Introduction";
+message.textContent = "JavaScript can change webpage content, react to user actions, and build interactive websites.";
