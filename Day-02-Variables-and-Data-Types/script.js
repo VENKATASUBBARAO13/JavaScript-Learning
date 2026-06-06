49c1529bcc9d9bf71db05f1d2630c5e3da6a865f
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-06 19:37:37 +0530

    Add files via upload

---
+++ Day-02-Variables-and-Data-Types/script.js
@@ -0,0 +1,16 @@
+﻿// Day 2: Variables and Data Types
+// Use const when a value should not change. Use let when it can change.
+
+const learnerName = "Bunny";
+let age = 18;
+let isLearning = true;
+
+console.log("Name:", learnerName);
+console.log("Age:", age);
+console.log("Learning JavaScript:", isLearning);
+
+document.getElementById("output").innerHTML = `
+    <p>Name: ${learnerName}</p>
+    <p>Age: ${age}</p>
+    <p>Learning JavaScript: ${isLearning}</p>
+`;
