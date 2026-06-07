98fc94166251cfe8f466b1df7eb2c8bbe0fba75d
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-07 10:34:45 +0530

    Add files via upload

---
+++ Day-03-Operators-and-Expressions/script.js
@@ -0,0 +1,22 @@
+﻿// Day 3: Operators and Expressions
+
+const numberOne = 20;
+const numberTwo = 6;
+
+const sum = numberOne + numberTwo;
+const difference = numberOne - numberTwo;
+const product = numberOne * numberTwo;
+const quotient = numberOne / numberTwo;
+const remainder = numberOne % numberTwo;
+const isGreater = numberOne > numberTwo;
+
+console.log(sum, difference, product, quotient, remainder, isGreater);
+
+document.getElementById("output").innerHTML = `
+    <p>${numberOne} + ${numberTwo} = ${sum}</p>
+    <p>${numberOne} - ${numberTwo} = ${difference}</p>
+    <p>${numberOne} * ${numberTwo} = ${product}</p>
+    <p>${numberOne} / ${numberTwo} = ${quotient}</p>
+    <p>${numberOne} % ${numberTwo} = ${remainder}</p>
+    <p>Is ${numberOne} greater than ${numberTwo}? ${isGreater}</p>
+`;
