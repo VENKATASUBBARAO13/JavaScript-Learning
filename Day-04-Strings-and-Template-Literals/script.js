2d6629d9a7d79ec27a64e8e1561e860995bb94f9
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-07 23:02:45 +0530

    Add files via upload

---
+++ Day-04-Strings-and-Template-Literals/script.js
@@ -0,0 +1,16 @@
+﻿// Day 4: Strings and Template Literals
+
+const firstName = "Bunny";
+const skill = "JavaScript";
+const sentence = `${firstName} is learning ${skill}.`;
+
+console.log(sentence);
+console.log(sentence.length);
+console.log(sentence.toUpperCase());
+
+document.getElementById("output").innerHTML = `
+    <p>Original: ${sentence}</p>
+    <p>Length: ${sentence.length}</p>
+    <p>Uppercase: ${sentence.toUpperCase()}</p>
+    <p>Does it include JavaScript? ${sentence.includes("JavaScript")}</p>
+`;
