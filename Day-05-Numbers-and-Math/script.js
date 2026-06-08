d325d7241311f9b20cb1e4942f859c091cb0fdca
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-08 21:26:33 +0530

    Add files via upload

---
+++ Day-05-Numbers-and-Math/script.js
@@ -0,0 +1,17 @@
+﻿// Day 5: Numbers and Math
+
+const price = 499.75;
+const roundedPrice = Math.round(price);
+const highest = Math.max(10, 35, 22, 90);
+const lowest = Math.min(10, 35, 22, 90);
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+console.log(roundedPrice, highest, lowest, randomNumber);
+
+document.getElementById("output").innerHTML = `
+    <p>Price: ${price}</p>
+    <p>Rounded Price: ${roundedPrice}</p>
+    <p>Highest Number: ${highest}</p>
+    <p>Lowest Number: ${lowest}</p>
+    <p>Random Number from 1 to 10: ${randomNumber}</p>
+`;
