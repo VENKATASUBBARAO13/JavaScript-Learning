0dcabdf48efba9959acca520c16b8d865a50752c
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-09 23:04:55 +0530

    Add files via upload

---
+++ Day-06-Conditionals/script.js
@@ -0,0 +1,21 @@
+﻿// Day 6: Conditionals
+
+const marks = 82;
+let grade;
+
+if (marks >= 90) {
+    grade = "A+";
+} else if (marks >= 75) {
+    grade = "A";
+} else if (marks >= 60) {
+    grade = "B";
+} else {
+    grade = "Keep practicing";
+}
+
+console.log("Grade:", grade);
+
+document.getElementById("output").innerHTML = `
+    <p>Marks: ${marks}</p>
+    <p>Grade: ${grade}</p>
+`;
