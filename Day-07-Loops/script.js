5eeccd0633c76ed8cde83b8e01387a6722d86c44
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-10 13:51:38 +0530

    Add files via upload

---
+++ Day-07-Loops/script.js
@@ -0,0 +1,18 @@
+﻿// Day 7: Loops
+
+let listItems = "";
+
+for (let day = 1; day <= 5; day++) {
+    listItems += `<li>Practice JavaScript for day ${day}</li>`;
+}
+
+let countdown = 3;
+while (countdown > 0) {
+    console.log("Countdown:", countdown);
+    countdown--;
+}
+
+document.getElementById("output").innerHTML = `
+    <h2>Practice Plan</h2>
+    <ul>${listItems}</ul>
+`;
