ad75d8b8b572a60766a6b15b89bb57095ed75ad2
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-11 22:29:12 +0530

    Add files via upload

---
+++ Day-08-Functions/script.js
@@ -0,0 +1,20 @@
+﻿// Day 8: Functions
+
+function greetLearner(name) {
+    return `Hello, ${name}! Keep learning JavaScript.`;
+}
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+const greeting = greetLearner("Bunny");
+const total = addNumbers(15, 25);
+
+console.log(greeting);
+console.log(total);
+
+document.getElementById("output").innerHTML = `
+    <p>${greeting}</p>
+    <p>15 + 25 = ${total}</p>
+`;
