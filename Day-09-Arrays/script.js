7812a0681dec884bc3216208a15ce067291729ce
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-12 23:06:50 +0530

    Add files via upload

---
+++ Day-09-Arrays/script.js
@@ -0,0 +1,16 @@
+﻿// Day 9: Arrays
+
+const skills = ["HTML", "CSS", "JavaScript"];
+skills.push("GitHub");
+
+const skillList = skills.map(function(skill) {
+    return `<li>${skill}</li>`;
+}).join("");
+
+console.log(skills);
+console.log("First skill:", skills[0]);
+
+document.getElementById("output").innerHTML = `
+    <p>Total Skills: ${skills.length}</p>
+    <ul>${skillList}</ul>
+`;
