cc7e0e0744e786ac4d5ff88a82af19115f6e35ea
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-13 11:16:03 +0530

    Add files via upload

---
+++ Day-10-Objects/script.js
@@ -0,0 +1,20 @@
+﻿// Day 10: Objects
+
+const learner = {
+    name: "Bunny",
+    course: "JavaScript Fundamentals",
+    progress: 10,
+    introduce: function() {
+        return `${this.name} is learning ${this.course}.`;
+    }
+};
+
+console.log(learner.name);
+console.log(learner.introduce());
+
+document.getElementById("output").innerHTML = `
+    <p>Name: ${learner.name}</p>
+    <p>Course: ${learner.course}</p>
+    <p>Current Day: ${learner.progress}</p>
+    <p>${learner.introduce()}</p>
+`;
