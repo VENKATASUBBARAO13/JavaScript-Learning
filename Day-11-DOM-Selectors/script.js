b38425cf4354298793e670745dac686d8bdc715a
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-14 22:30:20 +0530

    Add files via upload

---
+++ Day-11-DOM-Selectors/script.js
@@ -0,0 +1,12 @@
+﻿// Day 11: DOM Selectors
+// DOM means Document Object Model. JavaScript can select and change HTML elements.
+
+const heading = document.querySelector("h1");
+const info = document.querySelector(".info");
+const button = document.querySelector("button");
+
+heading.style.color = "#2563eb";
+info.textContent = "This text was selected and changed using JavaScript.";
+button.textContent = "DOM Button";
+
+console.log(heading, info, button);
