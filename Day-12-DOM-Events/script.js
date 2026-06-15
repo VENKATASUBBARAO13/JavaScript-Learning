8549dabbe03c212d2e03354d7c60dba3257554ca
VENKATA SUBBARAO <venkatasubbarao1305@gmail.com>
2026-06-15 23:09:46 +0530

    Add files via upload

---
+++ Day-12-DOM-Events/script.js
@@ -0,0 +1,10 @@
+﻿// Day 12: DOM Events
+
+const button = document.getElementById("change-button");
+const message = document.getElementById("message");
+let clicks = 0;
+
+button.addEventListener("click", function() {
+    clicks++;
+    message.textContent = `You clicked the button ${clicks} time(s).`;
+});
